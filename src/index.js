a7ca1032d7244003dd61268e53e1a1137f10757a
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-29 18:35:43 +0100

    Add all src for images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import "./style.css"
+import SlideOne from "./SlideOne.png"
+import SlideTwo from "./SlideTwo.png"
+import SlideThree from "./SlideThree.png"
+
+
+const track = document.querySelector(".imageTrack")
+const slides = document.querySelectorAll("li")
+
+const imageList = document.querySelectorAll(".image")
+
+imageList[0].src = SlideOne
+imageList[1].src = SlideTwo
+imageList[2].src = SlideThree
+
+
