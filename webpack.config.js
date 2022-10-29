b8a1e4c63d110bc023b4c6754ca2dd86e98cd233
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-29 18:21:53 +0100

    Initialize npm, download dependencies, fill html template

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        use: "svg-inline-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(js)$/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/index.html",
+    title: "... app",
+    filename: "index.html",
+  }),
+
+  ],
+  mode: "development",
+  devServer: {
+    port: 3900,
+    hot: true,
+
+  },
+
+};
