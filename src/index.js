03a41e9ccf6ade3f619da8134a4962a428d21dd4
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-29 18:41:26 +0100

    Align images next to each other and set widths of container

--- src/index.js
+++ src/index.js
@@ -6,8 +6,13 @@ import SlideThree from "./SlideThree.png"
 
 const track = document.querySelector(".imageTrack")
 const slides = document.querySelectorAll("li")
-
 const imageList = document.querySelectorAll(".image")
+
+const slideWidth = slides[0].getBoundingClientRect().width
+
+console.log(slides[0])
+
+console.log(slideWidth)
 
 imageList[0].src = SlideOne
 imageList[1].src = SlideTwo
