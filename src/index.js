c83a50464cd35402b4a7e3ebceef390cde9867b9
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2022-10-29 19:45:57 +0100

    Get nav buttons working using currentDot variable

--- src/index.js
+++ src/index.js
@@ -10,6 +10,9 @@ const imageList = document.querySelectorAll(".image")
 const nextButton = document.querySelector(".nextButton")
 const prevButton = document.querySelector(".prevButton")
 const slidesArray = Array.from(slides)
+const navButtons = document.querySelectorAll(".navButton")
+const navButtonsArray = Array.from(navButtons)
+
 
 
 const slideWidth = 400
@@ -21,22 +24,34 @@ nextButton.addEventListener("click", (e) => {
     if (nextSlide != null) 
     {
         
+        
 
         let amountToMove = nextSlide.getBoundingClientRect().width
         track.style.transform = `translateX(-${amountToMove * slidesArray.indexOf(nextSlide)}px)`
         currentSlide.classList.remove("currentSlide")
         nextSlide.classList.add("currentSlide")
+
+        let currentDot = navButtonsArray[slidesArray.indexOf(currentSlide)]
+        currentDot.classList.remove("currentSlide")
     
         currentSlide = nextSlide
         nextSlide = currentSlide.nextElementSibling
-        console.log(nextSlide)
+       
+        currentDot = navButtonsArray[slidesArray.indexOf(currentSlide)]
+        currentDot.classList.add("currentSlide")
+        
+        
     } else
     {
         let amountToMove = currentSlide.getBoundingClientRect().width
         track.style.transform = `translateX(0px)`
+        let currentDot = navButtonsArray[slidesArray.indexOf(currentSlide)]
+        currentDot.classList.remove("currentSlide")
         currentSlide.classList.remove("currentSlide")
         slidesArray[0].classList.add("currentSlide")
         currentSlide = slidesArray[0]
+        currentDot = navButtonsArray[slidesArray.indexOf(currentSlide)]
+        currentDot.classList.add("currentSlide")
 
         
     }
